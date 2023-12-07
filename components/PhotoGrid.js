36eeae225013e4e57dae5b6db77f2cfc5c6af247
cellity <celineperley@gmail.com>
2023-12-07 17:02:26 -0400

    removed stuff

--- components/PhotoGrid.js
+++ components/PhotoGrid.js
@@ -36,33 +36,6 @@ const PhotoGrid = () => {
     setDemons(data);
   }
 
-  /*useEffect(() => {
-    const client = new S3Client({
-      region: "us-east-1",
-      // Unless you have a public bucket, you'll need access to a private bucket.
-      // One way to do this is to create an Amazon Cognito identity pool, attach a role to the pool,
-      // and grant the role access to the 's3:GetObject' action.
-      //
-      // You'll also need to configure the CORS settings on the bucket to allow traffic from
-      // this example site. Here's an example configuration that allows all origins. Don't
-      // do this in production.s3clei
-      //[
-      //  {
-      //    "AllowedHeaders": ["*"],
-      //    "AllowedMethods": ["GET"],
-      //    "AllowedOrigins": ["*"],
-      //    "ExposeHeaders": [],
-      //  },
-      //]
-      //
-      credentials: {
-        accessKeyId: "AKIA4RWJVKE3WQU6T3NN",
-        secretAccessKey: "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",
-      },});
-    const command = new ListObjectsCommand({ Bucket: "mydemonbucket" });
-    client.send(command).then(({ Contents }) => setObjects(Contents || []));
-    console.log(objects)
-  }, []); */ 
 
   useEffect(() => {
     getDemonData();
