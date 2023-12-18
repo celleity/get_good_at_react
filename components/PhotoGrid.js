b4e3a2a1dfe16a872f6d5cc1813414f675b79a07
cellity <celineperley@gmail.com>
2023-12-18 15:51:32 -0400

    added black images

--- components/PhotoGrid.js
+++ components/PhotoGrid.js
@@ -1,4 +1,4 @@
-import react,{ useState, useEffect } from "react";
+import react,{ useState, useEffect, useMemo } from "react";
 import { createClient } from "@supabase/supabase-js";
 import ImageList from '@mui/material/ImageList';
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
@@ -14,6 +14,8 @@ import { fromCognitoIdentityPool } from "@aws-sdk/credential-providers";
 
 const PhotoGrid = () => {
 
+
+
   const supabase = createClient("https://mtlhlvwncdtbgxcbovei.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10bGhsdnduY2R0Ymd4Y2JvdmVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDExOTQzMDQsImV4cCI6MjAxNjc3MDMwNH0.3IlU9ziCnnPsGriA9axHtA2aG4IyLox07po4okFprP8");
   const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -23,37 +25,69 @@ const PhotoGrid = () => {
     color: theme.palette.text.secondary,
   }));
   
-  const [objects, setObjects] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [demons, setDemons] = useState([]);
   const s3url = "https://mydemonbucket.s3.amazonaws.com/";
   let demonNum = 1;
-
+ 
   async function getDemonData() {
+
+    
    
   const { data} = await supabase
     .from('demons')
     .select('*')
     .order('id', { ascending: true })
-    setDemons(data);
-  }
+    setDemons(data)
+    setIsLoading(false);
+   
 
+}
+const hundredDemons = useMemo(() => {
+  let newDemons = []
+  
+    if(demons.length <= 100 && !isLoading){
+     
+      const blackImg = [];
+     
+     
+      
+      for(let count = demons.length + 1; count <= 100; count++){
+      blackImg.push({id: count,  demonName: count, demonDescription: "this demon has not yet been immortalized", image: "https://mydemonbucket.s3.amazonaws.com/Ink+Demons/black.jpg"})
+        
+      }
+      newDemons = [...demons, ...blackImg]
+      console.log("newDemons", newDemons)
+    }
+    return newDemons
+
+}, [demons])
 
   useEffect(() => {
-    getDemonData();
-  }, []);
+   
+    getDemonData()
+    
+
+  
+  }
+  , []);
+
+
 
 // get demon data here? from db? and go through all -> if no name, then black image  src === null ? black.jpg : src.name
 //add s3url + image? 
 return (
+
     <Grid container  columns={{ xs: 20  }} spacing={0}>
       
-    {demons.map((o) => (
-      
+    {hundredDemons.map((o) => (
       <Grid xs={2} sm={4} md={4} key={o.id}> 
         <DemonCard demonNumber={{number:o.id}} demonSummary={{summary: o.demonDescription}} demonImageURL={o.image} demonName={o.demonName}/>
         
       </Grid>
     ))}
+      {  console.log(hundredDemons)}
+
     </Grid>
 );
 
